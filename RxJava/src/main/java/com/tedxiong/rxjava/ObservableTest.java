fca6738c2c46fbaa9a862a30e38218d5c1b83351
Ted <ted@lifang.com>
2018-06-24 15:16:56 +0800

    add rxjava

---
+++ RxJava/src/main/java/com/tedxiong/rxjava/ObservableTest.java
@@ -0,0 +1,12 @@
+package com.tedxiong.rxjava;
+
+/**
+ * Created by ted on 2018/6/24.
+ * in com.tedxiong.rxjava
+ */
+public class ObservableTest {
+
+    public void main(){
+
+    }
+}
---
+++ RxJava2/src/main/java/com/tedxiong/rxjava2/MainActivity.java
@@ -0,0 +1,52 @@
+package com.tedxiong.rxjava2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.tedxiong.rxjava.R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(com.tedxiong.rxjava.R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(com.tedxiong.rxjava.R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(com.tedxiong.rxjava.R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == com.tedxiong.rxjava.R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
